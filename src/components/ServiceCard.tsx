a15d7b7416ab1e29e06709c6c455b6952bef24a9
CalebeSG <calebeguimaraes10@gmail.com>
2023-08-18 18:52:06 -0300

    created service section

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,12 +1,16 @@
-import { TrendUp } from '@phosphor-icons/react'
+interface ServiceCardProps {
+  placeholder: string,
+  description: string,
+  children: React.ReactNode
+}
 
-export default function ServiceCard() {
+export default function ServiceCard({ children, placeholder, description }: ServiceCardProps) {
   return (
     <div className='flex flex-col items-center justify-center px-8 py-10 bg-gray-800 rounded-xl w-full'>
-      <TrendUp className='text-green-400 mb-10' size={80} />
-      <strong className='font-medium text-2xl text-gray-200'>Trafico</strong>
+      {children}
+      <strong className='font-medium text-2xl text-gray-200'>{placeholder}</strong>
       <span className='text-gray-400 text-center px-4 mt-4'>
-        Potêncializamos o alcance do seu negócio de forma acertiva
+        {description}
       </span>
     </div>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head"
 
+import { TrendUp } from '@phosphor-icons/react'
+
 import HeaderBar from "@/components/HeaderBar"
 import Container from "@/layout/Container"
 import ServiceCard from "@/components/ServiceCard"
@@ -15,36 +17,55 @@ export default function Home() {
 
       <main>
         <Container>
+          {/* {BANNER ==================================================} */}
           <div className="w-full relative flex items-center justify-center md:justify-start bg-banner bg-cover m-auto h-[74vh] px-6 -z-10 after:absolute after:w-full after:h-full after:bg-black/30 after:backdrop-blur-sm after:left-0 after:-z-20">
-            <section className="w-96">
-              <h1 className="text-white font-bold text-4xl leading-relaxed text-center md:text-left">
-                Levando seu comercio para o proximo nível
+            <section className="w-[50%]">
+              <h1 className="text-white font-bold text-5xl leading-relaxed text-center md:text-left">
+                Levando seu comercio para o próximo <span className="text-green-400">Nível</span>
               </h1>
               
-              <p className="text-gray-300 text-center mt-4 leading-8 md:text-left">
+              <p className="text-gray-300 text-lg pr-20 text-center mt-4 leading-8 md:text-left">
                 Aqui trabalhamos em varias frentes para 
                 tornar seu emprendimento vísivel para o maximo de clientes
               </p>
             </section>
           </div>
 
-          <section className="px-6 py-10">
-            <div>
-              <div>
-                <strong>Serviços</strong>
-                <h2>O que podemos fazer por você</h2>
+          {/* {SERVIÇOS ===================================================} */}
+
+          <section className="px-6 py-14">
+            <div className="flex">
+              <div className="flex-1">
+                <strong className="uppercase text-gray-600 font-light tracking-[6px]">Serviços</strong>
+                <h2 className="text-4xl font-bold text-gray-800">O que podemos fazer por você</h2>
               </div>
-              <p>
+              <p className="w-[40%] text-gray-800 font-semibold">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
                 Sequi est illum delectus sit, libero dolores quia unde impedit 
                 omnis animi praesentium eum quisquam perspiciatis aliquam et ipsa fugit. Eos, sed.
               </p>
             </div>
 
-            <div className="flex justify-between gap-10">
-              <ServiceCard />
-              <ServiceCard />
-              <ServiceCard />
+            <div className="flex justify-between gap-10 mt-14">
+              <ServiceCard 
+                placeholder="Trafico" 
+                description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi est illum" 
+              >
+                <TrendUp className='text-green-400 mb-10' size={80} />
+              </ServiceCard>
+              <ServiceCard 
+                placeholder="Trafico" 
+                description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi est illum" 
+              >
+                <TrendUp className='text-green-400 mb-10' size={80} />
+              </ServiceCard>
+              <ServiceCard 
+                placeholder="Trafico" 
+                description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi est illum" 
+              >
+                <TrendUp className='text-green-400 mb-10' size={80} />
+              </ServiceCard>
+             
             </div>
           </section>
         </Container>
